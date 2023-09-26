3eb9816f82157b88647d185ec57bb7f9de959f62
JI0PATA <wanja.shukov14@gmail.com>
2023-09-26 13:31:09 +0300

    Look around with camera

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import {Sky} from "@react-three/drei";
+import {PointerLockControls, Sky} from "@react-three/drei";
 import {Ground} from "./Ground.jsx";
 
 export const App = () => {
     return (
         <>
+            <PointerLockControls />
             <Sky sunPosition={[100, 20, 100]}/>
             <ambientLight intensity={1.5} />
             <Ground />
--- src/Ground.jsx
+++ src/Ground.jsx
@@ -9,7 +9,7 @@ export const Ground = () => {
     return (
         <mesh position={[0, -5, 0]} rotation-x={-Math.PI / 2}>
             <planeGeometry args={[500, 500]} />
-            <meshStandardMaterial color="gray" map={texture} map-repeat={[360, 360]} />
+            <meshStandardMaterial color="gray" map={texture} map-repeat={[100, 100]} />
         </mesh>
     );
 }
