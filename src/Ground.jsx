31fe05543209c6828b7d5b69f06189e9f48a124a
JI0PATA <wanja.shukov14@gmail.com>
2023-09-26 13:45:05 +0300

    Floor is a rigid body

--- src/Ground.jsx
+++ src/Ground.jsx
@@ -1,15 +1,18 @@
 import * as THREE from "three";
 import { useTexture } from "@react-three/drei";
 import floorTexture from "./assets/floor.png";
+import {RigidBody} from "@react-three/rapier";
 
 export const Ground = () => {
     const texture = useTexture(floorTexture);
     texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
 
     return (
-        <mesh position={[0, -5, 0]} rotation-x={-Math.PI / 2}>
-            <planeGeometry args={[500, 500]} />
-            <meshStandardMaterial color="gray" map={texture} map-repeat={[100, 100]} />
-        </mesh>
+        <RigidBody>
+            <mesh position={[0, -5, 0]} rotation-x={-Math.PI / 2}>
+                <planeGeometry args={[500, 500]} />
+                <meshStandardMaterial color="gray" map={texture} map-repeat={[100, 100]} />
+            </mesh>
+        </RigidBody>
     );
 }
