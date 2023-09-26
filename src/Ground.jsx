f1c91e9106b77f6c45b5edb48e3b229ba0e1862c
JI0PATA <wanja.shukov14@gmail.com>
2023-09-26 13:26:47 +0300

    Texture for the floor

--- src/Ground.jsx
+++ src/Ground.jsx
@@ -1,8 +1,15 @@
+import * as THREE from "three";
+import { useTexture } from "@react-three/drei";
+import floorTexture from "./assets/floor.png";
+
 export const Ground = () => {
+    const texture = useTexture(floorTexture);
+    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+
     return (
         <mesh position={[0, -5, 0]} rotation-x={-Math.PI / 2}>
             <planeGeometry args={[500, 500]} />
-            <meshStandardMaterial color="gray" />
+            <meshStandardMaterial color="gray" map={texture} map-repeat={[360, 360]} />
         </mesh>
     );
 }
