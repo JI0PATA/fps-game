050c0cc402ee0f4043e7c786db36afb10f07fa00
JI0PATA <wanja.shukov14@gmail.com>
2023-11-14 11:41:58 +0300

    New folder images; Change the path to image

--- src/Ground.jsx
+++ src/Ground.jsx
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import { useTexture } from "@react-three/drei";
-import floorTexture from "./assets/floor.png";
+import floorTexture from "./assets/images/floor.png";
 import {CuboidCollider, RigidBody} from "@react-three/rapier";
 
 export const Ground = () => {
